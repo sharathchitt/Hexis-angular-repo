e0105e87b94c60992eac6395e6b9a50566d53ea3
Verma79 <bhavnaverma1779@gmail.com>
2021-09-23 19:17:04 +0530

    Added admin-dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
+    AdminDashboardComponent,
     
   ],
   imports: [
