07399fdf4420d5424378542cb61d2c0c2f93c0cf
mridu4321 <mridula4321@gmail.com>
2021-09-23 19:06:09 +0530

    Added user-dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
+    UserDashboardComponent,
     
   ],
   imports: [
